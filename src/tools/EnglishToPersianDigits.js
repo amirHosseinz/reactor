100e66856cd7b6d59886a6efcadf3d87b15b8e2f
Amir HS <amirhossein.zarezadeh@gmail.com>
2018-01-26 14:04:41 +0330

    persian and arabic to english digits added

--- src/tools/EnglishToPersianDigits.js
+++ src/tools/EnglishToPersianDigits.js
@@ -14,3 +14,23 @@ export function englishToPersianDigits (input) {
   }
   return '';
 }
+
+export function persianArabicToEnglishDigits (input) {
+  if (typeof input !== "undefined") {
+    input = input.toString();
+    var result = '';
+    var perDiff = 1776 - 48;
+    var arDiff = 1632 - 48;
+    for (var i = 0; i < input.length; i++) {
+      if (input.charCodeAt(i) >= 1776 && input.charCodeAt(i) <= 1785 ) {
+        result = result + String.fromCharCode(input.charCodeAt(i) - perDiff);
+      } else if (input.charCodeAt(i) >= 1632 && input.charCodeAt(i) <= 1641 ) {
+        result = result + String.fromCharCode(input.charCodeAt(i) - arDiff);
+      } else {
+        result = result + input[i];
+      }
+    }
+    return result;
+  }
+  return '';
+}
