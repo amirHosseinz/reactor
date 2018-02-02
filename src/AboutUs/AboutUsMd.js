54cd91fba2fa9468cc5bf8ac4f5dfceaf9ccf07e
erfankorki <erfankorki@gmail.com>
2018-02-02 03:54:06 -0800

    about us refactored

---
+++ src/AboutUs/AboutUsMd.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {englishToPersianDigits} from "../tools/EnglishToPersianDigits.js";
+
+
+class AboutUsMd extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+    };
+  }
+  render(){
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default AboutUsMd;
---
+++ src/AboutUs/AboutUsXs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {englishToPersianDigits} from "../tools/EnglishToPersianDigits.js";
+
+class AboutUsXs extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+    };
+  }
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AboutUsXs;
