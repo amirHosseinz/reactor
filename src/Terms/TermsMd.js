0f367dfca5d5b13054f107b038d113b07c27b994
erfankorki <erfankorki@gmail.com>
2018-02-02 06:12:44 -0800

    terms refactored

---
+++ src/Terms/TermsMd.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Divider } from 'semantic-ui-react';
+import Collapsible from 'react-collapsible';
+import {englishToPersianDigits} from '../tools/EnglishToPersianDigits.js';
+
+
+class TermsMd extends React.Component{
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+export default TermsMd;
---
+++ src/Terms/TermsSm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Divider } from 'semantic-ui-react';
+import Collapsible from 'react-collapsible';
+import {englishToPersianDigits} from '../tools/EnglishToPersianDigits.js';
+
+
+class TermsSm extends React.Component{
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+export default TermsSm;
