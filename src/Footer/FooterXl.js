ded3d7dbcca49a617519dc33bc5caca925c8f3f1
alecferaa <alecferaa@gmail.com>
2018-02-11 16:01:33 +0330

    footer socials postions and style of search-bar suggests styled

--- src/Footer/FooterXl.js
+++ src/Footer/FooterXl.js
@@ -7,15 +7,19 @@ class FooterXl extends React.Component{
     return(
       <div className='footer navbar-fixed-bottom'>
         <div className='footerchild' >
-          <div className="footer-child-margined row-reverse"  dir="rtl">
-
+          <div className="footer-child-margined row-reverse" dir="rtl">
+            <div className="footer-social-menu col-lg-2">
                   <a href="https://t.me/tripinni"><img src={require('../Images/trypinn-telegram-channel.png')} className='social-image-xl' alt=""></img></a>
                   <a href="https://instagram.com/tripinn.ir"><img src={require('../Images/trypinn-instagram.png')} className='social-image-xl' alt=""></img></a>
                   <a href="https://twitter.com/tripinni"><img src={require('../Images/trypinn-twitter.png')} className='social-image-xl' alt=""></img></a>
-                  <div className='footer-menu'>
-                    © 2018, Tripinn, All Rights Reserved.
-                  </div>
             </div>
+
+            <div className='footer-copy-right col-lg-8'>
+              © 2018, Tripinn, All Rights Reserved.
+            </div>
+            <div className="col-lg-2">
+            </div>
+          </div>
         </div>
       </div>
     );
