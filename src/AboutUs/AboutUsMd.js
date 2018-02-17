2242b14949b549bfb409a20af71c43f4ddddcfe8
ghazalehran <34242245+ghazalehran@users.noreply.github.com>
2018-02-17 17:20:56 +0330

    about us updated

--- src/AboutUs/AboutUsMd.js
+++ src/AboutUs/AboutUsMd.js
@@ -18,35 +18,33 @@ class AboutUsMD extends React.Component{
         </div>
         <p className='about-us-header'><span>درباره  </span><span className='about-us-tripinn'>تریپین</span></p>
         <div className='about-us-main-paragraph'>
-        <p className="dabout-us-descriptions"> اگر برای سفر به دنبال اقامتگاهی مناسب هستید تریپین برای شما بهترین انتخاب است.
-      <br/>
-   تریپین سامانه رزرو ویلا، سوییت و اقامتگاه‌های بومگردی است که از طریق آن می‌توانید با مقایسه اقامتگاه‌های تفریحی گوناگون در شهرهای مختلف و رزرو آنلاین آنها قبل از سفر خود، اقامتی بی‌نظیر را تجربه کنید.
-      <br/>
-       در طول سفر نیز همراه شما خواهد بود و با پشتیبانی 24 ساعته شما را همراهی خواهد کرد. در صورت بروز سوال و پیشنهاد از طریق صفحه تماس با ما، ما را در جریان بگذارید.
-      <br/>
-       تریپین متعلق به شرکت طراحی و راهبری فناوران دانش سامان آریا به شماره ثبت {englishToPersianDigits(515951 )} است و در حال حاضر در استان های
-      <Link className="anchor-about-us" to="/search/مازندران" > مازندران</Link> ,
-      <Link className="anchor-about-us" to="/search/خراسان رضوی">خراسان رضوی</Link> ,
-      <Link className="anchor-about-us" to="/search/اصفهان">اصفهان</Link> ,
-      <Link className="anchor-about-us" to="/search/گیلان">گیلان</Link> و
-      <Link className="anchor-about-us" to="/search/کیش"> جزیره کیش</Link> فعال است.
-       </p>
-       </div>
-       <div className='about-us-app-paragraph'>
-         <div className='about-us-app-paragraph-text'>
-         علاوه بر سایت، شما میتوانید با اپلیکیشن تریپین نیز به رزرو انواع اقامتگاه ها بپردازید. هم اکنون اپلیکیشن تریپین برای هر دو پلتفرم اندروید و آی او اس قابل دریافت است.
-         </div>
-         <div className='about-us-app-paragraph-img '>
-          <img src={require('../Images/about-us-pic1.png')} height="250" />
-         </div>
-       </div>
-       <div className='about-us-tripinn-objective'>
-         <div className='about-us-tripinn-objective-img'>
-         <img src={require('../Images/about-us-pic2.png')} height="350" />
-         </div>
-         <div className='about-us-tripinn-objective-text'>
-        هدف تریپین ارائه تنوعی بالا از انواع ویلا، اقامتگاه بومگردی، سوییت و آپارتمان متناسب با نیاز شماست.
+        <p className="dabout-us-descriptions"> اگر به دنبال امکان مقایسه و بررسی اقامتگاه های تفریحی هستید، تریپین بهترین انتخاب شما است. پیش از آغاز سفر محل اقامت مورد نظر خود را آنلاین رزرو کنید و در طول مسافرت همراهی تیم پشتیبانی ما را در کنار خود داشته باشید. تریپین متعلق به شرکت طراحی و راهبری فناوران دانش سامان آریا به شماره ثبت {englishToPersianDigits(515951 )}  است و در حال حاضر در استان های
+
+       <Link className="anchor-about-us" to="/search/اصفهان">اصفهان </Link> ,
+       <Link className="anchor-about-us" to="/search/البرز">البرز</Link> ,
+       <Link className="anchor-about-us" to="/search/خراسان رضوی">خراسان رضوی</Link> ,
+       <Link className="anchor-about-us" to="/search/مازندران" > مازندران</Link> ,
+       <Link className="anchor-about-us" to="/search/گیلان">گیلان</Link> و
+       <Link className="anchor-about-us" to="/search/کیش"> جزیره کیش</Link> فعال است.
+        </p>
         </div>
+        <div className='about-us-app-paragraph'>
+          <div className='about-us-app-paragraph-text'>
+           با دریافت اپلیکیشن تریپین برای هر دو پلتفرم اندورید و آی‌او‌اس، علاوه بر رزرو آنلاین، به کمک امکان برقراری ارتباط مستقیم با میزبان، آسوده خاطر سفر کنید.
+
+          </div>
+          <div className='about-us-app-paragraph-img '>
+           <img src={require('../Images/about-us-pic1.png')} height="250" />
+          </div>
+        </div>
+        <div className='about-us-tripinn-objective'>
+          <div className='about-us-tripinn-objective-img'>
+          <img src={require('../Images/about-us-pic2.png')} height="350" />
+          </div>
+          <div className='about-us-tripinn-objective-text'>
+           تریپین با هدف ایجاد تجربه‌ای نو، تلاش دارد تا با ارائه‌ی طیفی وسیع از ویلا، سوییت و اقامتگاه‌های بوم‌گردی، پاسخگوی نیاز جامعه ایرانی باشد.
+
+         </div>
       </div>
     </div>
   </div>
