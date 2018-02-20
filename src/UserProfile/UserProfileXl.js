1c35bda02ba85d32b7fcf24d67384b64eee25a93
erfankorki <erfankorki@gmail.com>
2018-02-20 10:13:06 -0800

    user profile xl phase 2

--- src/UserProfile/UserProfileXl.js
+++ src/UserProfile/UserProfileXl.js
@@ -153,18 +153,23 @@ class UserProfileXl extends React.Component{
     if(this.state.profileInfo!==null){
       return(
         <div className="user-profile-in-details-main-division">
-          <div className="user-profile-profile-picture">
-          </div>
+        <div>
+        <img className="user-profile-profile-picture" height="200px" width="200px" src={'https://www.trypinn.com/'+this.state.profileInfo.user.profile_picture} />
+        </div>
           <div className="user-profile-user-name">
           </div>
           <hr/>
           <div className="user-profile-in-details-link">
+            پیام ها
           </div>
           <div className="user-profile-in-details-link">
+            مشاهده درخواست ها
           </div>
           <div className="user-profile-in-details-link">
+            مشاهده سفر‌ها
           </div>
           <div className="user-profile-in-details-link">
+            مکان های مورد علاقه
           </div>
         </div>
       );
@@ -174,17 +179,31 @@ class UserProfileXl extends React.Component{
     return(
       <div className="user-profile-edit-main-division">
         <div className="user-profile-edit-main-heading">
+          ویرایش حساب کاربری
         </div>
         <hr />
         <div className="user-profile-edit-secondary-heading`">
           <p className="user-profile-edit-secondary-heading-title">
+            مشخصات کاربری
           </p>
           <p className="user-profile-edit-secondary-heading-description">
+           بر اساس قوانین، اطلاعات مندرج شما در این سامانه محفوظ خواهد ماند.
           </p>
         </div>
         <div className="user-profile-edit-get-data-zone">
           <div className="user-profile-edit-input-paragraph">
             <span className="user-profile-edit-input-paragraph-title">
+            نام
+            </span>
+          </div>
+          <div>
+           <input className="user-profile-edit-input"/>
+          </div>
+        </div>
+        <div className="user-profile-edit-get-data-zone">
+          <div className="user-profile-edit-input-paragraph">
+            <span className="user-profile-edit-input-paragraph-title">
+             نام خانوادگی
             </span>
             <span className="user-profile-edit-input-paragraph-description">
             </span>
@@ -195,8 +214,10 @@ class UserProfileXl extends React.Component{
         <div className="user-profile-edit-get-data-zone">
           <div className="user-profile-edit-input-paragraph">
             <span className="user-profile-edit-input-paragraph-title">
+             شماره همراه
             </span>
             <span className="user-profile-edit-input-paragraph-description">
+          (از این شماره برای ورود به اپلیکیشن و سایت سامانه استفاده خواهد شد)
             </span>
           </div>
           <div className="user-profile-edit-input">
@@ -205,8 +226,10 @@ class UserProfileXl extends React.Component{
         <div className="user-profile-edit-get-data-zone">
           <div className="user-profile-edit-input-paragraph">
             <span className="user-profile-edit-input-paragraph-title">
+              کُد ملی
             </span>
             <span className="user-profile-edit-input-paragraph-description">
+          (ثبت کد ملی صحیح و منطبق با نام و نام خانوادگی برای رزرو ضروری می‌باشد)
             </span>
           </div>
           <div className="user-profile-edit-input">
@@ -215,18 +238,10 @@ class UserProfileXl extends React.Component{
         <div className="user-profile-edit-get-data-zone">
           <div className="user-profile-edit-input-paragraph">
             <span className="user-profile-edit-input-paragraph-title">
+            ایمیل
             </span>
             <span className="user-profile-edit-input-paragraph-description">
-            </span>
-          </div>
-          <div className="user-profile-edit-input">
-          </div>
-        </div>
-        <div className="user-profile-edit-get-data-zone">
-          <div className="user-profile-edit-input-paragraph">
-            <span className="user-profile-edit-input-paragraph-title">
-            </span>
-            <span className="user-profile-edit-input-paragraph-description">
+            (اختیاری)
             </span>
           </div>
           <div className="user-profile-edit-input">
@@ -235,15 +250,16 @@ class UserProfileXl extends React.Component{
         <hr />
         <div className="user-profile-edit-secondary-heading`">
           <p className="user-profile-edit-secondary-heading-title">
+            رمز عبور
           </p>
           <p className="user-profile-edit-secondary-heading-description">
+            در صورت نیاز به تغییر رمز عبور،این بخش را ویرایش کنید.در غیر اینصورت رمز عبور سابق شما ثبت شده می‌ماند
           </p>
         </div>
         <div className="user-profile-edit-get-data-zone">
           <div className="user-profile-edit-input-paragraph">
             <span className="user-profile-edit-input-paragraph-title">
-            </span>
-            <span className="user-profile-edit-input-paragraph-description">
+          : رمز عبور
             </span>
           </div>
           <div className="user-profile-edit-input">
@@ -252,8 +268,7 @@ class UserProfileXl extends React.Component{
         <div className="user-profile-edit-get-data-zone">
           <div className="user-profile-edit-input-paragraph">
             <span className="user-profile-edit-input-paragraph-title">
-            </span>
-            <span className="user-profile-edit-input-paragraph-description">
+             تکرار رمز عبور:
             </span>
           </div>
           <div className="user-profile-edit-input">
