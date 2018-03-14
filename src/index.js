a94ad5ba2f5d119ef54deda10febcf888d5886e3
erfankorki <erfankorki@gmail.com>
2018-03-14 10:54:27 -0700

    added react snap shot for testing pre rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import ReactDOMServer from 'react-dom/server';
+import {render} from 'react-snapshot';
 import MainPage from './MainPage.js';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-aspect-ratio/aspect-ratio.css';
@@ -14,4 +15,4 @@ class Main extends React.Component{
   }
 }
 
-ReactDOM.hydrate(<Main />, document.getElementById('root'));
+render(<Main />, document.getElementById('root'));
