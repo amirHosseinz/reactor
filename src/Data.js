43fe744a099edc90b13652a7907da3f01cf7bff6
erfankorki <erfankorki@gmail.com>
2018-05-28 13:23:37 +0430

    changed the source of api response

--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,3 @@
-// export const productionURL = "https://www.trypinn.com/";
-export const productionURL = "http://127.0.0.1:8000/";
+export const productionURL = "https://www.trypinn.com/";
+// export const productionURL = "http://127.0.0.1:8000/";
 export const productionMediaURL = "https://www.trypinn.com";
